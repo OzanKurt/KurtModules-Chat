80b0c099f96a0e3f118ce2b758ec83b48ed8daf9
Ozan Kurt <me@ozankurt.com>
2026-05-28 19:07:54 +0300

    chore(chat): copy Pint, PHPStan, Rector, PHPUnit, gitattributes/gitignore from Forum

---
+++ rector.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+
+return RectorConfig::configure()
+    ->withPaths([__DIR__.'/src', __DIR__.'/tests'])
+    ->withSets([
+        LevelSetList::UP_TO_PHP_84,
+        SetList::CODE_QUALITY,
+        SetList::DEAD_CODE,
+        SetList::TYPE_DECLARATION,
+    ]);
